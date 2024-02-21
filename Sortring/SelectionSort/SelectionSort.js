0b2187d7059e3f6f8bd06a6225e70511a1c4e240
nahidhossain123 <nahidhossain351@gmail.com>
2024-02-21 23:57:06 +0600

    selection sort

---
+++ Sortring/SelectionSort/SelectionSort.js
@@ -0,0 +1,15 @@
+const swap = () => {};
+
+const selectionSort = (arr, n) => {
+    for(let i=0;i<n;i++){
+        let min = arr[i]
+        for(let j=i+1;j<n;j++){
+            if(min<arr[j]){
+                let temp = 
+            }
+        }
+    }
+};
+
+let values = [10, 50, 30, 70, 80, 20, 90, 40];
+console.log("Longest Substring", LongestSubstring(values, values.length));
