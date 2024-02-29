7a25944a3cfad429d6252156a2d2c7bb4aa21342
nahidhossain123 <nahidhossain351@gmail.com>
2024-02-29 14:04:25 +0600

    linked list data structure

---
+++ LinkedList/LinkedList.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  addFirst(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  addLast(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = newNode;
+  }
+
+  size() {
+    let count = 0;
+    let currentNode = this.head;
+    while (currentNode.next) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return count;
+  }
+
+  addAt(index, data) {
+    if (index < 0 || index > this.size()) {
+      console.error("Invalid Index");
+      return;
+    }
+    const newNode = new Node(data);
+    if (index == 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    let currentNode = this.Node;
+    for (let i = 0; i < index - 1; i++) {
+      currentNode = currentNode.next;
+    }
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+  }
+  print() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.data);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+let list = new LinkedList();
+
+list.addFirst(5);
+list.addFirst(6);
+list.addFirst(7);
+list.addFirst(8);
+list.print();
